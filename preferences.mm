01b9f3ebd27736afb7d6a0efb3b1843eb437bbaa
Samuel Maddock <smaddock@salesforce.com>
2022-06-15 16:00:59 -0400

    refactor: remove old getter methods

--- preferences.mm
+++ preferences.mm
@@ -180,65 +180,8 @@ Napi::Value GetValue(const Napi::CallbackInfo &info) {
 
   Napi::Value value = PropertyToValue(info.Env(), property_list);
   CFRelease(property_list);
+
   return value;
-}
-
-Napi::Value GetString(const Napi::CallbackInfo &info) {
-  if (info.Length() < 1) {
-    throw Napi::Error::New(info.Env(), "Missing 'key' argument to GetString()");
-  }
-
-  CFStringRef key = GetCFKeyFromArgs(info);
-  CFStringRef value_str = (CFStringRef)CFPreferencesCopyAppValue(
-      key, kCFPreferencesCurrentApplication);
-  CFRelease(key);
-
-  if (!value_str) {
-    return Napi::Value();
-  }
-
-  Napi::Value value =
-      Napi::Value::From(info.Env(), CFStringToStdString(value_str));
-  CFRelease(value_str);
-  return value;
-}
-
-Napi::Value GetInteger(const Napi::CallbackInfo &info) {
-  if (info.Length() < 1) {
-    throw Napi::Error::New(info.Env(),
-                           "Missing 'key' argument to GetInteger()");
-  }
-
-  CFStringRef key = GetCFKeyFromArgs(info);
-  Boolean value_existed = false;
-  CFIndex value_int = CFPreferencesGetAppIntegerValue(
-      key, kCFPreferencesCurrentApplication, &value_existed);
-  CFRelease(key);
-
-  if (!value_existed) {
-    return Napi::Value();
-  }
-
-  return Napi::Value::From(info.Env(), value_int);
-}
-
-Napi::Value GetBoolean(const Napi::CallbackInfo &info) {
-  if (info.Length() < 1) {
-    throw Napi::Error::New(info.Env(),
-                           "Missing 'key' argument to GetBoolean()");
-  }
-
-  CFStringRef key = GetCFKeyFromArgs(info);
-  Boolean value_existed = false;
-  Boolean value_bool = CFPreferencesGetAppBooleanValue(
-      key, kCFPreferencesCurrentApplication, &value_existed);
-  CFRelease(key);
-
-  if (!value_existed) {
-    return Napi::Value();
-  }
-
-  return Napi::Value::From(info.Env(), CFBooleanToBool(value_bool));
 }
 
 Napi::Value IsPreferenceForced(const Napi::CallbackInfo &info) {
@@ -260,12 +203,6 @@ Napi::Value IsPreferenceForced(const Napi::CallbackInfo &info) {
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
   exports.Set(Napi::String::New(env, "getValue"),
               Napi::Function::New(env, GetValue));
-  exports.Set(Napi::String::New(env, "getString"),
-              Napi::Function::New(env, GetString));
-  exports.Set(Napi::String::New(env, "getInteger"),
-              Napi::Function::New(env, GetInteger));
-  exports.Set(Napi::String::New(env, "getBoolean"),
-              Napi::Function::New(env, GetBoolean));
 
   exports.Set(Napi::String::New(env, "isPreferenceForced"),
               Napi::Function::New(env, IsPreferenceForced));
