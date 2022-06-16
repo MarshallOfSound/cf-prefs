5728156152c4ec0d080848a868bba795370377a0
Samuel Maddock <smaddock@salesforce.com>
2022-06-16 11:27:36 -0400

    chore: update types

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,6 @@
 // Type definitions for cf-prefs
 // Project: cf-prefs
 
-export function getPreferenceValue(key: string, type: 'string'): string | undefined;
-export function getPreferenceValue(key: string, type: 'string', defaultValue: string): string;
-export function getPreferenceValue(key: string, type: 'integer'): number | undefined;
-export function getPreferenceValue(key: string, type: 'integer', defaultValue: number): number;
-export function getPreferenceValue(key: string, type: 'boolean'): boolean | undefined;
-export function getPreferenceValue(key: string, type: 'boolean', defaultValue: boolean): boolean;
+export function getPreferenceValue(key: string): any;
 
 export function isPreferenceForced(key: string): boolean;
