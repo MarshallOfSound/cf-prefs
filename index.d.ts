87b7da4db8d1b35cb93013565d92302e76191193
Samuel Maddock <smaddock@salesforce.com>
2022-06-21 18:14:14 -0400

    fix: circular type error (#2)

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for cf-prefs
 // Project: cf-prefs
 
-export type CFPrefValue = boolean | number | string | Array<CFPrefValue> | Record<string, CFPrefValue> | undefined;
+export type CFPrefValue = boolean | string | number | { [key: string]: CFPrefValue } | Array<CFPrefValue>;
 export function getPreferenceValue(key: string): CFPrefValue;
 
 export function isPreferenceForced(key: string): boolean;
